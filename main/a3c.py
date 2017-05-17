926a71280d0c462ac6144bd8fd4de8a3b49a6361
mystiking <maxkragballe@gmail.com>
2017-05-17 18:30:15 +0200

    FINAL version of a3c (for cartpole)

--- main/a3c.py
+++ main/a3c.py
@@ -15,7 +15,7 @@ import random
 import time
 import threading
 
-results = open('results_cp_11.csv', 'w')
+
 
 # Constants
 ALPHA = 0.5
@@ -40,10 +40,6 @@ output_actor = Dense(NUM_ACTIONS,activation='softmax',kernel_initializer='unifor
 # The "final" networks
 global_critic_model = Model(inputs=input_layer, outputs=output_critic)
 global_actor_model = Model(inputs=input_layer, outputs=output_actor)
-# Here the optimizer is irrelevant since we won't be using the Keras
-# function "fit" to optimize the model
-#global_critic_model.compile(optimizer='sgd', loss='mse')
-#global_actor_model.compile(optimizer='sgd', loss='mse')
 # Must be done to enable threading
 global_critic_model._make_predict_function()
 global_actor_model._make_predict_function()
@@ -55,11 +51,13 @@ init = tf.global_variables_initializer()
 sess.run(init)
 graph = tf.get_default_graph()
 
+start_time = time.time()
 
 class Agent(threading.Thread):
-    def __init__(self, state_input_shape, action_output_shape):
+    def __init__(self, state_input_shape, action_output_shape, agent_id):
         threading.Thread.__init__(self)
-        self.opt = tf.train.RMSPropOptimizer(0.005)
+        self.results = open('a3c_run_1_agent_{}.csv'.format(agent_id), 'w')
+        self.step_counter = 1
         self.gtheta = 0
         self.gw = 0
         # Init layers of the model
@@ -71,10 +69,6 @@ class Agent(threading.Thread):
         # The "final" networks
         self.local_critic_model = Model(inputs=input_layer, outputs=output_critic)
         self.local_actor_model = Model(inputs=input_layer, outputs=output_actor)
-        # Here the optimizer is irrelevant since we won't be using the Keras
-        # function "fit" to optimize the model
-        #self.local_critic_model.compile(optimizer='sgd', loss='categorical_crossentropy')
-        #self.local_actor_model.compile(optimizer='sgd', loss='categorical_crossentropy')
         # Must be done to enable threading
         self.local_critic_model._make_predict_function()
         self.local_actor_model._make_predict_function()
@@ -100,11 +94,12 @@ class Agent(threading.Thread):
         self.c_grads = tf.gradients(loss_critic, w_critic)
 
     def run(self):
-        self.train(200, 4000)
+        self.train(5, 2000)
         print("It worked!")
 
-    def train(self, t, episodes):
+    def train(self, t_max, episodes):
         i = 0
+        t_start = self.step_counter
         env = gym.make(GAME_NAME)
         while i < episodes: 
             i += 1
@@ -116,6 +111,7 @@ class Agent(threading.Thread):
                 start_weights_critic = global_critic_model.get_weights()
                 self.local_critic_model.set_weights(start_weights_critic)
             lock.release()
+
             dtheta = 0
             dw = 0
             total_reward = 0
@@ -136,44 +132,52 @@ class Agent(threading.Thread):
                 total_reward += reward
                 #td_error = reward + GAMMA * _value - value
                 memory.append([state, action, reward, done])
-
                 state = _state
                 value = _value
-            print("Episode {} : Reward = {}".format(i, total_reward))
-            results.write(str(i) + ',' + str(total_reward) + '\n')
-            reward_list.append(total_reward)
-            R = 0
-            for s, a, r, d in memory:
-                R = r + GAMMA * R
-                a_list = np.zeros(NUM_ACTIONS)
-                a_list[a] = 1.
-                a_list = np.reshape(a_list, (-1, 2))
-                actor_grads = sess.run(self.a_grads, {self.s_t : s, self.a_t : a_list, self.r_t : np.reshape(np.array([R]), (-1, 1))})
-                dtheta = dtheta + np.array(actor_grads)
+                self.step_counter += 1
+                if done or self.step_counter - t_start == t_max:
+                    R = 0 if done else self.local_critic_model.predict(state)
+                    memory = reversed(memory)
+                    for s, a, r, d in memory:
+                        R = r + GAMMA * R
+                        a_list = np.zeros(NUM_ACTIONS)
+                        a_list[a] = 1.
+                        a_list = np.reshape(a_list, (-1, 2))
+                        actor_grads = sess.run(self.a_grads, {self.s_t : s, self.a_t : a_list, self.r_t : np.reshape(np.array([R]), (-1, 1))})
+                        dtheta = dtheta + np.array(actor_grads)
 
-                critic_grads = sess.run(self.c_grads, {self.s_t : s, self.r_t : np.reshape(np.array([R]), (-1, 1))})
-                dw = dw + np.array(critic_grads)
-            lock.acquire()
-            with graph.as_default():
-                self.gtheta = self.gtheta * ALPHA + (1 - ALPHA) * (dtheta**2)
-                self.gw = self.gw * ALPHA + (1 - ALPHA) * (dw**2)
-                j = 0
-                while j < len(dtheta):
-                    # Critic
-                    update_critic = global_critic_model.layers[j+1].get_weights()[0] - LR * dw[j] / np.sqrt(self.gw[j] + EPSILON)
-                    bias_critic = global_critic_model.layers[j+1].get_weights()[1]
-                    global_critic_model.layers[j+1].set_weights((update_critic, bias_critic))
-                    # Actor
-                    update_actor = global_actor_model.layers[j+1].get_weights()[0] - LR * dtheta[j] / np.sqrt(self.gtheta[j] + EPSILON)
-                    bias_actor = global_actor_model.layers[j+1].get_weights()[1]
-                    global_actor_model.layers[j+1].set_weights((update_actor, bias_actor))
-                    j += 1
-            lock.release()
+                        critic_grads = sess.run(self.c_grads, {self.s_t : s, self.r_t : np.reshape(np.array([R]), (-1, 1))})
+                        dw = dw + np.array(critic_grads)
+                    lock.acquire()
+                    with graph.as_default():
+                        self.gtheta = self.gtheta * ALPHA + (1 - ALPHA) * (dtheta**2)
+                        self.gw = self.gw * ALPHA + (1 - ALPHA) * (dw**2)
+                        j = 0
+                        while j < len(dtheta):
+                            # Critic
+                            update_critic = global_critic_model.layers[j+1].get_weights()[0] - LR * dw[j] / np.sqrt(self.gw[j] + EPSILON)
+                            bias_critic = global_critic_model.layers[j+1].get_weights()[1]
+                            global_critic_model.layers[j+1].set_weights((update_critic, bias_critic))
+                            # Actor
+                            update_actor = global_actor_model.layers[j+1].get_weights()[0] - LR * dtheta[j] / np.sqrt(self.gtheta[j] + EPSILON)
+                            bias_actor = global_actor_model.layers[j+1].get_weights()[1]
+                            global_actor_model.layers[j+1].set_weights((update_actor, bias_actor))
+                            j += 1
+                    lock.release()
+                    dw = 0
+                    dtheta = 0
+                    t_start = self.step_counter
+                    memory = []
+
+            now = time.time()
+            self.results.write(str(i) + ',' + str(total_reward) + ',' + str(now - start_time) + '\n')
+            reward_list.append(total_reward)
+            
         
-agent006 = Agent(NUM_STATE, NUM_ACTIONS)
-agent007 = Agent(NUM_STATE, NUM_ACTIONS)
-agent008 = Agent(NUM_STATE, NUM_ACTIONS)
-agent009 = Agent(NUM_STATE, NUM_ACTIONS)
+agent006 = Agent(NUM_STATE, NUM_ACTIONS, 1)
+agent007 = Agent(NUM_STATE, NUM_ACTIONS, 2)
+agent008 = Agent(NUM_STATE, NUM_ACTIONS, 3)
+agent009 = Agent(NUM_STATE, NUM_ACTIONS, 4)
 
 agent006.start()
 agent007.start()
@@ -185,18 +189,18 @@ agent007.join()
 agent008.join()
 agent009.join()
 
-c = 0
-k = 500
-avg_list = []
-for i in range(len(reward_list) - 1):
-    if c % k == 0:
-        avg_list.append(sum(reward_list[i + c : i + c + k]) / k)
+#c = 0
+#k = 500
+#avg_list = []
+#for i in range(len(reward_list) - 1):
+#    if c % k == 0:
+#        avg_list.append(sum(reward_list[i + c : i + c + k]) / k)
 
-print(sum(reward_list) / len(reward_list))
-plt.plot(range(len(reward_list)), reward_list, 'r')
-plt.plot(range(len(avg_list)), avg_list, 'b')
-plt.xlabel('Episodes')
-plt.ylabel('Score')
-plt.title('Score of the a3c algorithm over time')
-plt.show()
+#print(sum(reward_list) / len(reward_list))
+#plt.plot(range(len(reward_list)), reward_list, 'r')
+#plt.plot(range(len(avg_list)), avg_list, 'b')
+#plt.xlabel('Episodes')
+#plt.ylabel('Score')
+#plt.title('Score of the a3c algorithm over time')
+#plt.show()
 
